c56eafc04f4315240e63728cf50050e5959509cc
Bartosz Badowski <bartoszbad@gmail.com>
2019-08-06 21:46:31 +0200

    recipe list activated

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -3,9 +3,18 @@ from datetime import datetime
 from django.shortcuts import render
 from django.views import View
 
+from jedzonko.models import Recipe
+
 
 class IndexView(View):
 
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
+
+class RecipeList(View):
+
+    def get(self, request):
+        recipes = Recipe.objects.all()
+        return render(request, "recipes.html", context={"recipes": recipes})
--- scrumlab/urls.py
+++ scrumlab/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from jedzonko.views import IndexView
+from jedzonko.views import IndexView, RecipeList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', IndexView.as_view()),
+    path('recipe/list', RecipeList.as_view())
 ]
